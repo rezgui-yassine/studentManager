298fc04713d6e4b5e6f1da351130e18c0d6d572a
yassine <rezguiyassine224@gmail.com>
2024-08-05 21:50:10 +0100

    add compteur for number of table

--- src/Test.java
+++ src/Test.java
@@ -4,19 +4,28 @@ import java.sql.*;
 public class Test {
 
     public static void connect() {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentManager", "root", "root");
-            System.out.println("Connection to Store DB successful!");
-            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-            ResultSet resultSet = statement.executeQuery("select * from student");
-          while(resultSet.next()){
-              System.out.println(resultSet.getInt("StudentID"));
-              System.out.println(resultSet.getString("FirstName"));
+       try{
+           Connection connection =DriverManager.getConnection("jdbc:mysql://localhost:3306/studentManager","root","root");
+              System.out.println("Connection successful");
+              //create a statement object for executing SQL queries
+              Statement statement = connection.createStatement();
+              //execute a query to get a result set from the database table student
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM student");
 
-          }
+                int compteur = 0;
+           while (resultSet.next()) {
+                compteur++;
+               System.out.println(String.format("%-15s %-15s %-15s %-15s",
+                       resultSet.getInt("StudentID"),
+                       resultSet.getString("FirstName"),
+                       resultSet.getString("LastName"),
+                       resultSet.getString("DateOfBirth")));
 
-        } catch (SQLException e) {
-            System.err.println("Connection failed: " + e.getMessage());
-        }
+           }
+           System.out.println("Number of students: "+compteur);
+       }catch (SQLException e){
+           System.err.println("Connection failed"+e.getMessage());
+
+       }
     }
 }
